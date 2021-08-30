9aa52d0c198582cf0080d7359cc7318ccd6c233e
Diêgo Ferreira <diegofrr.19@gmail.com>
2021-08-30 08:33:19 -0300

    Update camara_deputados.py

--- camara_deputados/camara_deputados.py
+++ camara_deputados/camara_deputados.py
@@ -10,22 +10,22 @@ lista_dados = r.json().get('dados')
 if (len(lista_dados) == 0):
 	print('Nenhum deputado encontrado')
 
-for deputado in lista_dados:
-	nome = deputado.get('nome')
-	siglaPartido = 'Não cadastrado'
-	if (deputado.get('siglaPartido') is not None):
-		siglaPartido = deputado.get('siglaPartido')
-	siglaUF = deputado.get('siglaUf')
-	email = 'Não cadastrado'
+else:
+	for deputado in lista_dados:
+		nome = deputado.get('nome')
+		siglaPartido = 'Não cadastrado'
+		if (deputado.get('siglaPartido') is not None):
+			siglaPartido = deputado.get('siglaPartido')
+		siglaUF = deputado.get('siglaUf')
+		email = 'Não cadastrado'
 
-	if (deputado.get('email') is not None):
-		email = deputado.get('email')
+		if (deputado.get('email') is not None):
+			email = deputado.get('email')
 
-	print(
-		f'{nome}, Partido: {siglaPartido}\n'
-		f'UF: {siglaUF}\n'
-		f'E-mail: {email}\n'
-		)
+		print(
+			f'{nome}, Partido: {siglaPartido}\n'
+			f'UF: {siglaUF}\n'
+			f'E-mail: {email}\n'
+			)
 
-if (len(lista_dados) > 0):
 	print(f'Foram encontrados {len(lista_dados)} deputados com o nome {nome_procurar}.')
